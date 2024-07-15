99d43d183c81c98a0819c7fc0dafd6f3cd1c8f6f
Pasha <psmolov@nautilusdigital.com>
2024-07-15 16:05:24 -0400

    13.Slice - multidimensional slice

---
+++ 104-slice-multidimensional-slice/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	jb := []string{"James", "Bond", "Chocolate"}
+	jm := []string{"Jenny", "Moneypenny", "Guiness", "Wolverine Tracks"}
+
+	fmt.Println(jb)
+	fmt.Println(jm)
+
+	xxs := [][]string{jb, jm}
+	fmt.Println(xxs)
+}
