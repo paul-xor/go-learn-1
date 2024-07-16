262c13bc13bbf4be5a988fef6024d467669675e6
Pasha <psmolov@nautilusdigital.com>
2024-07-16 19:55:19 -0400

    15. Map intro

---
+++ 115-map-intro/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	am := map[string]int{
+		"Todd":   42,
+		"Henry":  16,
+		"Padget": 14,
+	}
+
+	fmt.Println("The age of Henry was ", am["Henry"])
+	fmt.Println(am)
+	fmt.Printf("%#v\n", am)
+
+	an := make(map[string]int)
+	an["Lucas"] = 28
+	an["Steph"] = 37
+	fmt.Println(an)
+	fmt.Printf("%#v\n", an)
+}
