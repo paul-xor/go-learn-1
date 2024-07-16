090625259a3a7b646f69ac629fa83c378523eba9
Pasha <psmolov@nautilusdigital.com>
2024-07-16 13:32:45 -0400

    14. Hands on #44

---
+++ 110-hands-on-44/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	xi := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
+
+	// [inclusive:exclusive]
+	fmt.Println(xi[:5])
+	fmt.Println(xi[5:])
+	fmt.Println(xi[2:7])
+	fmt.Println(xi[1:6])
+}
+
+/*
+[42 43 44 45 46]
+[47 48 49 50 51]
+[44 45 46 47 48]
+[43 44 45 46 47]
+*/
