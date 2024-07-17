89a675b93cf59b6e41fae813f7775cf89699cb1f
Pasha <psmolov@nautilusdigital.com>
2024-07-16 20:14:10 -0400

    15. Map - delete element

---
+++ 117-map-delete-element/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	an := make(map[string]int)
+	an["Lucas"] = 28
+	an["Steph"] = 37
+	an["George"] = 78
+	fmt.Println(an)
+	fmt.Printf("%#v\n", an)
+
+	delete(an, "George")
+
+	for k, v := range an {
+		fmt.Printf("map: k = %v, v = %v\n", k, v)
+	}
+}
