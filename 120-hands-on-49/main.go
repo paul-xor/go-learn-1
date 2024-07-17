b08da75932b8320867f2f7d962f61f9dd6c5205c
Pasha <psmolov@nautilusdigital.com>
2024-07-17 12:32:57 -0400

    16. Hands on #49

---
+++ 120-hands-on-49/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	mx := make(map[string][]string)
+	mx["bond_james"] = []string{"shaken, not stirred", "martinis", "fast cars"}
+	mx["moneypenny_jenny"] = []string{"intellegence", "literature", "computer_science"}
+	mx["no_dr"] = []string{"cats", "ice cream", "sunsets"}
+
+	fmt.Printf("jb = %#v\n", mx)
+	for i, v := range mx {
+		for n, m := range v {
+			fmt.Printf("index:%v\t value: %v\t feature_ind: %v\t features: %v\n", i, v, n, m)
+		}
+	}
+}
