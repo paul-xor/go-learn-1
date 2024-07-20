1b315655b2af9f402404c86425a7c541d613096a
Pasha <psmolov@nautilusdigital.com>
2024-07-19 21:07:09 -0400

    18. Hands on #55

---
+++ 130-hands-on-55/main.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+type engine struct {
+	electric bool
+}
+
+type vehicle struct {
+	engine
+	make  string
+	model string
+	doors int
+	color string
+}
+
+func main() {
+	v1 := vehicle{
+		engine: engine{
+			electric: true,
+		},
+		make:  "Ford",
+		model: "Mustang",
+		doors: 4,
+		color: "Blue",
+	}
+
+	v2 := vehicle{
+		engine: engine{
+			electric: false,
+		},
+		make:  "Toyota",
+		model: "Tundra",
+		doors: 2,
+		color: "White",
+	}
+
+	fmt.Println(v1)
+	fmt.Println(v2)
+
+	fmt.Println(v1.electric, v1.make, v1.model)
+	fmt.Println(v2.electric, v2.make, v2.model)
+
+	fmt.Println(v1.engine.electric, v1.make, v1.model)
+	fmt.Println(v2.engine.electric, v2.make, v2.model)
+
+}
