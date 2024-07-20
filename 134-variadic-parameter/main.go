8578fa25c6f11d7ca42cab55f9de7b7fc6827a48
Pasha <psmolov@nautilusdigital.com>
2024-07-20 12:49:20 -0400

    19. Functions - variadic parameter

---
+++ 134-variadic-parameter/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := sum(1, 3, 5)
+	fmt.Println("The sum is ", s)
+}
+
+func sum(ii ...int) int {
+	fmt.Println(ii)
+	fmt.Printf("%T\n", ii)
+
+	n := 0
+	for _, v := range ii {
+		n += v
+	}
+	return n
+}
+
+// func (r reciver) idetifier(p prarmeters(s) (return(s)) {<code here>}
