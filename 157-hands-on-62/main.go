7d669e8b2b0ec03c19c2b28b3577deba96ff4780
Pasha <psmolov@nautilusdigital.com>
2024-07-26 20:18:58 -0400

    20. Hands on #62

---
+++ 157-hands-on-62/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type square struct {
+	length float64
+	width  float64
+}
+
+type circle struct {
+	radius float64
+}
+
+func (s square) area() float64 {
+	return s.length * s.width
+}
+
+func (c circle) area() float64 {
+	return math.Pi * math.Pow(c.radius, 2)
+}
+
+type shape interface {
+	area() float64
+}
+
+func info(s shape) float64 {
+	return s.area()
+}
+
+func main() {
+	c1 := circle{
+		radius: 5,
+	}
+
+	s1 := square{
+		length: 5,
+		width:  4,
+	}
+
+	fmt.Println(info(c1))
+	fmt.Println(info(s1))
+}
