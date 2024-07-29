cf9f6c2131c82502842302e5a91e37f4ec798d20
Pasha <psmolov@nautilusdigital.com>
2024-07-29 18:05:42 -0400

    20. Hands on #69

---
+++ 164-hands-on-69/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	x()
+
+	y := func() {
+		for i := 0; i < 10; i++ {
+			fmt.Println(i)
+		}
+	}
+
+	y()
+
+}
+
+var x = func() {
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+}
