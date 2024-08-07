a20cc2118cadb35eccf934379eb8c52023ce9dd5
Pasha <psmolov@nautilusdigital.com>
2024-08-06 20:37:58 -0400

    25.Ninja - hands on #2, unmarshal

---
+++ 196-hands-on-#2/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Car struct {
+	Model  string `json:"Model"`
+	Make   string `json:"Make"`
+	Type   string `json:"Type"`
+	Year   int    `json:"Year"`
+	Engine string `json:"Engine"`
+}
+
+func main() {
+	s := `[{"Model": "Pathfinder", "Make": "Nissan", "Type": "SUV", "Year": 2021, "Engine": "V8"}, {"Model": "Cayenne", "Make": "Porche", "Type": "SUV", "Year": 2023, "Engine": "V8"}]`
+	bs := []byte(s)
+
+	fmt.Println(bs)
+	var vehicles []Car
+	err := json.Unmarshal(bs, &vehicles)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("cars: ", vehicles)
+
+	for i, v := range vehicles {
+		fmt.Println("\n Car number: ", i)
+		fmt.Println(v.Model, v.Make, v.Year)
+	}
+}
