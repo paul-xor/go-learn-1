5c369afb97e03aa0d0f1faba09038dbd7a7c20e5
Pasha <psmolov@nautilusdigital.com>
2024-08-09 20:30:43 -0400

    27.Hands on #4, mutex

---
+++ 210-hands-on-#4/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup
+
+	incrementor := 0
+	gs := 100
+	wg.Add(gs)
+	var m sync.Mutex
+
+	for i := 0; i < gs; i++ {
+		go func() {
+			m.Lock()
+			v := incrementor
+			runtime.Gosched()
+			v++
+			incrementor = v
+			fmt.Println(incrementor)
+			m.Unlock()
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	fmt.Println("end value", incrementor)
+}
