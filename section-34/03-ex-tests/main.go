9f5213b5c3d5497ddb7ffeb40fe3aeab8dcb1959
Pasha <psmolov@nautilusdigital.com>
2024-08-20 21:42:13 -0400

    34.Testing & banchmarking - example tests

---
+++ section-34/03-ex-tests/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"whole_learn_module/section-34/03-ex-tests/acdc"
+)
+
+func main() {
+	fmt.Println(acdc.Sum(2, 3))
+	fmt.Println(acdc.Sum(2, 3, 4, 5, 6, 7, 8, 9))
+}
